b7c82df571ae3438ef72426f1c27a873e9fdeea0
Soumik Mukherjee <60540758+soumik-mukherjee@users.noreply.github.com>
2024-06-20 17:52:34 +0530

    changes done and tested

---
+++ internal/configs/config.go
@@ -0,0 +1,35 @@
+package configs
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/spf13/viper"
+)
+
+type resource struct {
+	Host        string
+	Listen_port string
+}
+type configuration struct {
+	Fe resource
+}
+
+var Config *configuration
+
+func NewConfiguration() (*configuration, error) {
+	viper.AddConfigPath("data")
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AutomaticEnv()
+	viper.SetEnvKeyReplacer(strings.NewReplacer(`.`, `_`))
+	err := viper.ReadInConfig()
+	if err != nil {
+		return nil, fmt.Errorf("error loading config file: %s", err)
+	}
+	err = viper.Unmarshal(&Config)
+	if err != nil {
+		return nil, fmt.Errorf("error reading config file: %s", err)
+	}
+	return Config, nil
+}
